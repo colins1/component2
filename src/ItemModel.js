98f7fa2f9eea4345123045721bfbdfda48580223
colins1 <maxrozhkov1@gmail.com>
2023-01-26 09:17:16 +0300

    aa

---
+++ src/ItemModel.js
@@ -0,0 +1,12 @@
+class ItemModel{
+    constructor(brand,title,description,descriptionFull,currency,price)
+    {
+        this.brand = brand;
+        this.title = title;
+        this.description = description;
+        this.descriptionFull = descriptionFull;
+        this.currency = currency;
+        this.price = price;
+    }
+}
+export default ItemModel;
